1a6a899c30980bd8c583d19b2fdbea320db74ab6
Chrs-creyk <chrisacount117@gmail.com>
2022-11-19 17:50:07 -0500

    testing

--- test/main.h
+++ test/main.h
@@ -1,10 +1,11 @@
-#ifndef HEADER_FILE_H
-#define HEADER_FILE_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 
