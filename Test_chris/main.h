b3e0cd9e53417f2a3aa785cfcea25bb44a9dfc04
Chrs-creyk <chrisacount117@gmail.com>
2022-11-21 19:55:46 -0500

    adding chrs

---
+++ Test_chris/main.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct fmt
+{
+    char *type;
+    int (*f)();
+} fmt_t;
+int _printf(const char *format, ...);
+int print_op(const char *format, fmt_t *print_arr, va_list cases);
+int print_char(va_list ch);
+int print_string(va_list str);
+int print_number(va_list nums);
+
+#endif
